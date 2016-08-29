d8d18874c055d18caa0bda01f234dcd15c1b41e9
Chris Amanse <chris@chrisamanse.xyz>
2016-08-29 19:24:05 +0800

    Add Digest+Common for common functions
    - such as preprocessing message

---
+++ Sources/Digests/Digest+Common.swift
@@ -0,0 +1,52 @@
+//
+//  Digest+Common.swift
+//  CryptoKit
+//
+//  Created by Chris Amanse on 29/08/2016.
+//
+//
+
+import Foundation
+
+public extension Digest {
+    public enum Endianess {
+        case bigEndian
+        case littleEndian
+    }
+    
+    public static func preprocess(message: Data, forLength length: Int, in endianess: Endianess) -> Data {
+        // Create mutable copy of message
+        var messageCopy = message
+        
+        // Append 1-bit and zeros until length of messageCopy in bits = 448 (mod 512)
+        // 448 bits = 56 bytes, 512 bits = 64 bytes
+        messageCopy.append(0x80)
+        
+        // Compute padded zeros count
+        var paddedZerosCount = messageCopy.count % length
+        
+        let target = length - 8 // 8-bytes for length
+        if paddedZerosCount <= 56 {
+            paddedZerosCount = target - paddedZerosCount
+        } else {
+            paddedZerosCount = length - paddedZerosCount + target
+        }
+        
+        // Append zeros
+        messageCopy.append(contentsOf: Array(repeating: UInt8(0x00), count: paddedZerosCount))
+        
+        // Append origin length in bits as 8-byte Data
+        let data: Data = {
+            switch endianess {
+            case .bigEndian:
+                return Data(from: (UInt64(message.count) * 8).bigEndian)
+            case .littleEndian:
+                return Data(from: (UInt64(message.count) * 8))
+            }
+        }()
+        
+        messageCopy.append(data)
+        
+        return messageCopy
+    }
+}
