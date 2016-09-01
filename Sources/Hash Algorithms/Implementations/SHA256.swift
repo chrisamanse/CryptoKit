b957bdc4cfea2de667d8589224a5d9f2c4ceb66c
Chris Amanse <chris@chrisamanse.xyz>
2016-09-01 15:37:19 +0800

    Use BaseUnit type in protocol extension instead
    - instead of UInt32

--- Sources/Hash Algorithms/Implementations/SHA256.swift
+++ Sources/Hash Algorithms/Implementations/SHA256.swift
@@ -9,6 +9,8 @@
 import Foundation
 
 public struct SHA256: SHA2Variant {
+    public typealias BaseUnit = UInt32
+    
     public static var endianess: Endianess {
         return .bigEndian
     }
--- Sources/Hash Algorithms/Implementations/SHA2Variant.swift
+++ Sources/Hash Algorithms/Implementations/SHA2Variant.swift
@@ -21,7 +21,7 @@ public protocol SHA2Variant: HashAlgorithm, HashPreprocessor {
 }
 
 public extension SHA2Variant {
-    public static var kConstants: [UInt32] {
+    public static var kConstants: [BaseUnit] {
         return [
             0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
             0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -34,12 +34,13 @@ public extension SHA2Variant {
         ]
     }
     
-    public static var initialHashValues: [UInt32] {
+    public static var initialHashValues: [BaseUnit] {
         return [
             0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
             0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
         ]
     }
+    
     
     public static func generateHashValues(from message: Data) -> [BaseUnit] {
         let paddedMessage = self.preprocess(message: message)
