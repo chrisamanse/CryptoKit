be857a9f775b3922338b5e1f04264a70927a396e
Chris Amanse <chris@chrisamanse.xyz>
2016-09-28 16:40:43 +0800

    Create HashFunctionTests

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,6 +3,7 @@ import XCTest
 @testable import CryptoKitTests
 
 XCTMain([
+     testCase(HashFunctionTests.allTests),
      testCase(MD5Tests.allTests),
      testCase(SHA1Tests.allTests),
      testCase(SHA2Tests.allTests)
