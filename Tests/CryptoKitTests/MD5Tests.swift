ee6ae53999e5cc0e7138371c611175ddd8933855
Chris Amanse <chris@chrisamanse.xyz>
2017-07-05 21:46:47 +0800

    Create empty MD5Tests

---
+++ Tests/CryptoKitTests/MD5Tests.swift
@@ -0,0 +1,16 @@
+//
+//  MD5Tests.swift
+//  CryptoKitTests
+//
+//  Created by Chris Amanse on 7/5/17.
+//
+
+import XCTest
+import CryptoKit
+
+class MD5Tests: XCTestCase {
+    static var allTests : [(String, (MD5Tests) -> () throws -> Void)] {
+        return [
+        ]
+    }
+}
