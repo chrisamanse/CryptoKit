412af8ab626130ceb843a999885600935f22ccff
Chris Amanse <chris@chrisamanse.xyz>
2017-07-05 21:41:12 +0800

    Remove all hashing implementation

--- CryptoKit.playground/Contents.swift
+++ CryptoKit.playground/Contents.swift
@@ -4,10 +4,3 @@ import Foundation
 import CryptoKit
 
 // Try CryptoKit API here...
-
-let message = "a".data(using: .utf8)!
-
-let hash = message.digest(using: .md5).map { String(format: "%02x", $0) }.joined()
-
-hash
-hash == "0cc175b9c0f1b6a831c399e269772661"
